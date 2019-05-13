25277f96b27d32124d1853a7046b9415facf8a70
Jason Schutz <jschutz@dignitastech.com>
2019-05-13 17:48:17 -0400

    Pull in a fix to AsyncCoroutineHelper to speeds up model loading

--- UnityGLTF/Assets/UnityGLTF/Scripts/Async/AsyncCoroutineHelper.cs
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Async/AsyncCoroutineHelper.cs
@@ -5,41 +5,18 @@ using System.Threading.Tasks;
 using UnityEngine;
 namespace UnityGLTF
 {
-	public interface IAsyncCoroutineHelper
-	{
-		Task RunAsTask(IEnumerator coroutine, string name);
-		Task YieldOnTimeout(string msg = null);
-	}
 
-	public class AsyncCoroutineHelper : MonoBehaviour, IAsyncCoroutineHelper
+    public class AsyncCoroutineHelper : MonoBehaviour
 	{
 		public float BudgetPerFrameInSeconds = 0.01f;
 
-		private Queue<CoroutineInfo> _actions = new Queue<CoroutineInfo>();
-		private WaitForEndOfFrame _waitForEndOfFrame = new WaitForEndOfFrame();
-		private float _timeout;
+        private WaitForEndOfFrame _waitForEndOfFrame = new WaitForEndOfFrame();
+
         public bool verbose = false;
         public float frameOverrunTimeReport = .1f;
+        private float _timeout;
 
-		public Task RunAsTask(IEnumerator coroutine, string name)
-		{
-			TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
-			lock (_actions)
-			{
-				_actions.Enqueue(
-					new CoroutineInfo
-					{
-						Coroutine = coroutine,
-						Tcs = tcs,
-						Name = name
-					}
-				);
-			}
-
-			return tcs.Task;
-		}
-
-		public async Task YieldOnTimeout(string msg = null)
+        public async Task YieldOnTimeout(string msg = null)
 		{
 			if (Time.realtimeSinceStartup > _timeout)
 			{
@@ -54,56 +31,24 @@ namespace UnityGLTF
                 int frame = Time.frameCount;
                 //await RunAsTask(EmptyYieldEnum(), nameof(EmptyYieldEnum));
                 await Task.Delay(TimeSpan.FromMilliseconds(1));
-			}
+                _timeout = Time.realtimeSinceStartup + BudgetPerFrameInSeconds;
+            }
 		}
 
-		private void Start()
-		{
-			_timeout = Time.realtimeSinceStartup + BudgetPerFrameInSeconds;
-		}
+        private void Start()
+        {
+            _timeout = Time.realtimeSinceStartup + BudgetPerFrameInSeconds;
 
-		private void Update()
-		{
-			StartCoroutine(ResetFrameTimeout());
+            StartCoroutine(ResetFrameTimeout());
+        }
 
-			CoroutineInfo ? coroutineInfo = null;
-
-			lock (_actions)
-			{
-				if (_actions.Count > 0)
-				{
-					coroutineInfo = _actions.Dequeue();
-				}
-			}
-
-			if (coroutineInfo != null)
-			{
-				StartCoroutine(CallMethodOnMainThread(coroutineInfo.Value));
-			}
-		}
-
-		private IEnumerator CallMethodOnMainThread(CoroutineInfo coroutineInfo)
-		{
-			yield return coroutineInfo.Coroutine;
-			coroutineInfo.Tcs.SetResult(true);
-		}
-
-		private IEnumerator EmptyYieldEnum()
-		{
-			yield break;
-		}
-
-		private IEnumerator ResetFrameTimeout()
-		{
-			yield return _waitForEndOfFrame;
-			_timeout = Time.realtimeSinceStartup + BudgetPerFrameInSeconds;
-		}
-
-		private struct CoroutineInfo
-		{
-			public IEnumerator Coroutine;
-			public TaskCompletionSource<bool> Tcs;
-			public string Name;
-		}
-	}
+        private IEnumerator ResetFrameTimeout()
+        {
+            while (true)
+            {
+                yield return _waitForEndOfFrame;
+                _timeout = Time.realtimeSinceStartup + BudgetPerFrameInSeconds;
+            }
+        }
+    }
 }
